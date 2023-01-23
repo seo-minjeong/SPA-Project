08b5fd34a7adf712a9a85747254c3c411bbbe723
seominjeong <smj12938@gmail.com>
2023-01-23 14:25:06 +0900

    docs: 폴더구조 변경

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const PORT = 3000;
-const Router = require("./router");
 
-app.use(express.static("public")); // 미들웨어 내장모듈기능. 지정된 디렉토리(public)에서 이미지, CSS 파일 및 JavaScript 파일을 제공.
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // 전송받은 데이터를 json으로 받겠다는 설정
+app.use("/static", express.static(path.resolve(__dirname, "client", "static")));
 
-app.set("view engine", "ejs"); // ejs 선언
-
-app.use("/", Router);
-// Router.get("/", (req, res) => {
-//   res.locals.title = "Node Chat";
-//   res.render("index");
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("PAGE NOT FOUND");
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
-app.listen(PORT);
+app.listen(3000, () => console.log("server on"));
