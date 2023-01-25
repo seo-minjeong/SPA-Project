40a8c6e10dacd2c339a4d2528cfc93f631213114
seominjeong <smj12938@gmail.com>
2023-01-26 02:58:56 +0900

    fix: 로컬 변경

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
-app.listen(3000, () => console.log("server on"));
+app.listen(7960, () => console.log("server on"));
